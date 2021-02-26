facc2628efcf9c000fdaee4dde488f3f62dfdbf4
Plamen P <pit_sofia@hotmail.com>
2021-02-26 17:56:37 +0200

    add new tasks
    Signed-off-by: Plamen P. <pit_sofia@hotmail.com>

---
+++ C# OOP/Inheritance-Lab/05.Stack-of-Strings/StackOfStrings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+
+        public Stack<string> AddRange(IEnumerable<string> someText)
+        {
+            foreach (var item in someText)
+            {
+                this.Push(item);
+            }
+            return this;
+        }
+    }
+}
