4b11cd4268327453583a5cd0ccb40e6068892c4d
Plamen P <pit_sofia@hotmail.com>
2021-02-26 15:01:02 +0200

    C# OOP Lab uploads
    Signed-off-by: Plamen P. <pit_sofia@hotmail.com>

---
+++ C# OOP/Inheritance-Lab/04.Random-List/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList
+                (new string[] { "zdrasti", "kak si","chao", "leka vecher", "dobur den", "blagodarq"});
+
+            Console.WriteLine(list.Count);
+
+            Console.WriteLine(list.RandomString());
+
+            Console.WriteLine(list.RandomString());
+
+            Console.WriteLine(list.RandomString());
+
+            Console.WriteLine(list.Count);
+
+            Console.WriteLine(string.Join(", ", list));
+
+        }
+    }
+}
---
+++ C# OOP/Inheritance-Lab/04.Random-List/RandomList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random rnd;
+
+        public RandomList(IEnumerable<string> collection) 
+            : base(collection)
+        {
+            rnd = new Random();
+        }
+
+        public string RandomString()
+        {
+            string result = string.Empty;
+            int index = rnd.Next(0, this.Count);
+            result = this[index];
+            this.RemoveAt(index);
+
+            return result;
+        }
+    }
+}
