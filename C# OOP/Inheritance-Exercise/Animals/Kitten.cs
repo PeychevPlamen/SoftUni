a4f4523ba6d6fc36fa3a16dbb418039563f3661b
Plamen P <pit_sofia@hotmail.com>
2021-03-02 13:41:14 +0200

    C# OOP - Exercises add tasks
    Signed-off-by: Plamen P. <pit_sofia@hotmail.com>

---
+++ C# OOP/Inheritance-Exercise/Animals/Kitten.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Kitten : Cat
+    {
+        private const string CurrGender = "Female";
+        public Kitten(string name, int age)
+            : base(name, age, CurrGender)
+        {
+
+        }
+
+        public override string ProduceSound()
+        {
+            return "Meow";
+        }
+    }
+}
--- C# OOP/Inheritance-Exercise/Animals/StartUp.cs
+++ C# OOP/Inheritance-Exercise/Animals/StartUp.cs
@@ -1,10 +1,81 @@
-﻿namespace Animals
+﻿using System;
+
+namespace Animals
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Beast!")
+                {
+                    break;
+                }
+
+                string[] commands = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = commands[0];
+                int age = int.Parse(commands[1]);
+                string gender = commands[2];
+
+                if (string.IsNullOrEmpty(name) || age < 0 || string.IsNullOrEmpty(gender))
+                {
+                    Console.WriteLine("Invalid input!");
+
+                    continue;
+                }
+
+                if (input == "Dog")
+                {
+                    Dog dog = new Dog(name, age, gender);
+
+                    //Console.WriteLine(dog);
+                    //Console.WriteLine(dog.ProduceSound());
+
+                    Console.WriteLine(dog);
+                }
+                else if (input == "Cat")
+                {
+                    Cat cat = new Cat(name, age, gender);
+
+                    //Console.WriteLine(cat);
+                    //Console.WriteLine(cat.ProduceSound());
+
+                    Console.WriteLine(cat);
+                }
+                else if (input == "Frog")
+                {
+                    Frog frog = new Frog(name, age, gender);
+
+                    //Console.WriteLine(frog);
+                    //Console.WriteLine(frog.ProduceSound());
+
+                    Console.WriteLine(frog);
+                }
+                else if (input == "Kittens")
+                {
+                    Kitten kittens = new Kitten(name, age);
+
+                    //Console.WriteLine(kittens);
+                    //Console.WriteLine(kittens.ProduceSound());
+
+                    Console.WriteLine(kittens);
+                }
+                else if (input == "Tomcat")
+                {
+                    Tomcat tomcat = new Tomcat(name, age);
+
+                    //Console.WriteLine(tomcat);
+                    //Console.WriteLine(tomcat.ProduceSound());
+
+                    Console.WriteLine(tomcat);
+                }
+               
+            }
         }
     }
 }
