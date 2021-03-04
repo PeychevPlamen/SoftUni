255a6b48f6f6dfb5b9bc195bcecf372727e26116
Plamen P <pit_sofia@hotmail.com>
2021-03-04 17:49:28 +0200

    C# OOP - Lab add tasks
    Signed-off-by: Plamen P. <pit_sofia@hotmail.com>

--- C# OOP/Encapsulation-Lab/Encapsulation-Lab/02.Salary/Program.cs
+++ C# OOP/Encapsulation-Lab/Encapsulation-Lab/02.Salary/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace PersonsInfo
 {
@@ -10,18 +9,19 @@ namespace PersonsInfo
         {
             var lines = int.Parse(Console.ReadLine());
             var persons = new List<Person>();
-
             for (int i = 0; i < lines; i++)
             {
                 var cmdArgs = Console.ReadLine().Split();
-                var person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]));
+                var person = new Person(cmdArgs[0],
+                                        cmdArgs[1],
+                                        int.Parse(cmdArgs[2]),
+                                        decimal.Parse(cmdArgs[3]));
+
                 persons.Add(person);
             }
-
-            persons.OrderBy(p => p.FirstName)
-                   .ThenBy(p => p.Age)
-                   .ToList()
-                   .ForEach(p => Console.WriteLine(p.ToString()));
+            var parcentage = decimal.Parse(Console.ReadLine());
+            persons.ForEach(p => p.IncreaseSalary(parcentage));
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
 
         }
     }
---
+++ C# OOP/Encapsulation-Lab/Encapsulation-Lab/04.Team/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+
+            int numOfPlayers = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numOfPlayers; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string firstName = input[0];
+                string lastName = input[1];
+                int age = int.Parse(input[2]);
+                decimal salary = decimal.Parse(input[3]);
+
+                Person person = new Person ( firstName, lastName, age, salary );
+
+                persons.Add(person);
+            }
+
+            Team team = new Team("SoftUni");
+
+            foreach (Person person in persons)
+            {
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+        }
+    }
+}
