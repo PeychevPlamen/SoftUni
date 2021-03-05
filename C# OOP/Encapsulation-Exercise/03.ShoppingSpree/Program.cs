b66b95d65a4992adb8792fabb6bb5372e8a7e1ce
Plamen P <pit_sofia@hotmail.com>
2021-03-05 23:33:35 +0200

    C# OOP - Encapsulation - Exercise add tasks
    Signed-off-by: Plamen P. <pit_sofia@hotmail.com>

--- C# OOP/Encapsulation-Exercise/03.ShoppingSpree/Program.cs
+++ C# OOP/Encapsulation-Exercise/03.ShoppingSpree/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace _03.ShoppingSpree
 {
@@ -8,69 +7,90 @@ namespace _03.ShoppingSpree
     {
         static void Main(string[] args)
         {
-            List<Person> people = new List<Person>();
-
-            List<Product> products = new List<Product>();
-
+            Dictionary<string, Person> people;
+            Dictionary<string, Product> products;
 
             try
             {
-                people = Console.ReadLine()
-                            .Split(';', StringSplitOptions.RemoveEmptyEntries)
-                            .Select(t => t.Split('='))
-                            .Select(t => new Person(t[0], decimal.Parse(t[1])))
-                            .ToList();
-
-
-                products = Console.ReadLine()
-                          .Split(';', StringSplitOptions.RemoveEmptyEntries)
-                          .Select(t => t.Split('='))
-                          .Select(t => new Product(t[0], decimal.Parse(t[1])))
-                          .ToList();
+                people = ReadPeople();
+                products = ReadProduct();
             }
             catch (ArgumentException ex)
             {
-
                 Console.WriteLine(ex.Message);
                 return;
             }
+            
 
+            string input = Console.ReadLine();
 
-
-
-
-
-            string input = string.Empty;
-
-            while ((input = Console.ReadLine()) != "END")
+            while (input != "END")
             {
+                string[] inputCmd = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
-                string[] tokens = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string personName = inputCmd[0];
+                string productName = inputCmd[1];
 
-                Person customer = people.First(p => p.Name == tokens[0]);
-                Product purchase = products.First(products => products.Name == tokens[1]);
-
+                Person person = people[personName];
+                Product product = products[productName];
                 try
                 {
-                    customer.AddProduct(purchase);
-                    Console.WriteLine();
+                    person.AddProduct(product);
+
+                    Console.WriteLine($"{personName} bought {productName}");
                 }
                 catch (ArgumentException ex)
                 {
-
                     Console.WriteLine(ex.Message);
-                    continue;
+                    
                 }
+                
 
-
+                input = Console.ReadLine();
             }
 
-
-            foreach (var person in people)
+            foreach (var person in people.Values)
             {
                 Console.WriteLine(person);
             }
+        }
 
+        private static Dictionary<string, Product> ReadProduct()
+        {
+            Dictionary<string, Product> result = new Dictionary<string, Product>();
+
+            string[] parts = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var part in parts)
+            {
+                string[] productData = part.Split("=", StringSplitOptions.RemoveEmptyEntries);
+
+                string productName = productData[0];
+                decimal productCost = decimal.Parse(productData[1]);
+
+                result[productName] = new Product(productName, productCost);
+            }
+            return result;
+        }
+
+        private static Dictionary<string, Person> ReadPeople()
+        {
+            Dictionary<string, Person> result = new Dictionary<string, Person>();
+
+            string[] parts = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var part in parts)
+            {
+                string[] personData = part.Split("=", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = personData[0];
+                decimal money = decimal.Parse(personData[1]);
+
+                result[name] = new Person(name, money);
+
+            }
+
+            return result;
         }
     }
 }
