c6cc762612e98fb84ef2c03e2ee762d76b2b40df
Plamen P <pit_sofia@hotmail.com>
2021-03-09 16:30:48 +0200

    C# OOP Lab uploads tasks
    Signed-off-by: Plamen P. <pit_sofia@hotmail.com>

---
+++ C# OOP/InterfacesAndAbstraction-Lab/01.Shapes/Rectangle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public int Width
+        {
+            get => this.width;
+            private set
+            {
+                this.width = value;
+            }
+        }
+        public int Height
+        {
+            get => this.height;
+            private set
+            {
+                this.height = value;
+            }
+        }
+        public void Draw()
+        {
+            DrawLine(Width, '*', '*');
+            for (int i = 1; i < Height - 1; i++)
+            {
+                DrawLine(Width, '*', ' ');
+            }
+            DrawLine(Width, '*', '*');
+        }
+
+        private void DrawLine(int width, char end, char mid)
+        {
+            Console.Write(end);
+
+            for (int i = 1; i < width - 1; i++)
+            {
+                Console.Write(mid);
+            }
+            Console.WriteLine(end);
+        }
+    }
+}
