c768a9f65fa5c1b8bc0f19aba891d1801be7bc83
Plamen P <pit_sofia@hotmail.com>
2021-03-12 13:06:51 +0200

    C# OOP Interfaces and Abstraction - Exercise add tasks
    Signed-off-by: Plamen P. <pit_sofia@hotmail.com>

---
+++ C# OOP/InterfacesAndAbstraction-Exercise/02.MultipleImplementation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PersonInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string id = Console.ReadLine();
+            string birthdate = Console.ReadLine();
+            IIdentifiable identifiable = new Citizen(name, age, id, birthdate);
+            IBirthable birthable = new Citizen(name, age, id, birthdate);
+            IPerson person = new Citizen(name, age, id, birthdate);
+
+            Console.WriteLine();
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Age);
+         
+            Console.WriteLine(identifiable.Id);
+            Console.WriteLine(birthable.Birthdate);
+
+        }
+    }
+}
