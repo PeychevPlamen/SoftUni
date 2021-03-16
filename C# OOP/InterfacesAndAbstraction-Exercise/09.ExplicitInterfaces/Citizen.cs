bbd0f56d071c5db754b784d985a5be7db0e80fe2
Plamen P <pit_sofia@hotmail.com>
2021-03-16 16:24:25 +0200

    C# OOP Interfaces and Abstraction - Exercise add tasks
    Signed-off-by: Plamen P. <pit_sofia@hotmail.com>

---
+++ C# OOP/InterfacesAndAbstraction-Exercise/09.ExplicitInterfaces/Citizen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09.ExplicitInterfaces
+{
+    public class Citizen : IPerson, IResident
+    {
+        public Citizen(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+
+        public string Country { get; private set; }
+
+        public string Age { get; private set; }
+
+        string IPerson.GetName()
+        {
+            return Name;
+        }
+        string IResident.GetName()
+        {
+            return $"Mr/Ms/Mrs {Name}";
+        }
+    }
+}
---
+++ C# OOP/InterfacesAndAbstraction-Exercise/09.ExplicitInterfaces/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09.ExplicitInterfaces
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            List<Citizen> citizen = new List<Citizen>();
+
+            while (input != "End")
+            {
+                string[] tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Citizen name = new Citizen(tokens[0]);
+                citizen.Add(name);
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in citizen)
+            {
+                Console.WriteLine(((IPerson)item).GetName());
+                Console.WriteLine(((IResident)item).GetName());
+            }
+            
+
+        }
+    }
+}
