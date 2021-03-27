e4e647c6f2eb09c885bd109e3bcfd2dd030138dd
Plamen P <pit_sofia@hotmail.com>
2021-03-27 19:57:49 +0200

    C# OOP Unit Testing Exercise upload tasks
    Signed-off-by: Plamen P. <pit_sofia@hotmail.com>

--- C# OOP/UnitTesting-Exercises/CarManager.Tests/CarTests.cs
+++ C# OOP/UnitTesting-Exercises/CarManager.Tests/CarTests.cs
@@ -1,18 +1,171 @@
 using NUnit.Framework;
+using System;
 
 namespace Tests
 {
     public class CarTests
     {
+        private string make;
+
+        private string model;
+
+        private double fuelConsumption;
+
+        private double fuelCapacity;
+
+
         [SetUp]
         public void Setup()
         {
+            make = "Make";
+            model = "Model";
+            fuelConsumption = 10;
+            fuelCapacity = 100;
+
+            CarManager.Car car = new CarManager.Car(make, model, fuelConsumption, fuelCapacity);
         }
 
         [Test]
-        public void Test1()
+        [TestCase("make ", "Model", 0, 100)]
+        [TestCase(null, "Model", 10, 100)]
+        [TestCase("make ", "", 10, 100)]
+        [TestCase("make ", "Model", -10, 100)]
+        [TestCase("make ", "Model", 10, 0)]
+        public void Ctor_CheckIfThrowException(string make, string model, double fuelConsumption, double fuelCapacity)
         {
-            Assert.Pass();
+            Assert.Throws<ArgumentException>(() => new CarManager.Car(make, model, fuelConsumption, fuelCapacity));
+        }
+
+        [Test]
+        public void Test_Car_Make_IsInvalid_ThrowException()
+        {
+            make = null;
+
+            Assert.Throws<ArgumentException>(() => new CarManager.Car(make, model, fuelConsumption, fuelCapacity));
+        }
+
+        [Test]
+        public void Check_Car_Make_IsValid()
+        {
+            make = "BMW";
+
+            string expectedMake = make;
+
+            CarManager.Car car = new CarManager.Car(make, model, fuelConsumption, fuelCapacity);
+
+            Assert.AreEqual(expectedMake, car.Make);
+
+        }
+
+        [Test]
+        public void Check_Car_Model_IsValid()
+        {
+            model = "X3";
+
+            string expectedModel = model;
+
+            CarManager.Car car = new CarManager.Car(make, model, fuelConsumption, fuelCapacity);
+
+            Assert.AreEqual(expectedModel, car.Model);
+
+        }
+
+        [Test]
+        [TestCase(15)]
+        [TestCase(77)]
+        [TestCase(5)]
+        public void FuelConsumption_IsValid(double fuel)
+        {
+            fuelConsumption = fuel;
+
+            double expectedFuelConsuption = fuel;
+
+            CarManager.Car car = new CarManager.Car(make, model, fuelConsumption, fuelCapacity);
+
+            Assert.AreEqual(expectedFuelConsuption, car.FuelConsumption);
+        }
+
+        [Test]
+        public void FuelCapacity_IsValid()
+        {
+            fuelCapacity = 99;
+
+            double expectedFuelCapacity = fuelCapacity;
+
+            Assert.AreEqual(expectedFuelCapacity, new CarManager.Car(make, model, fuelConsumption, fuelCapacity).FuelCapacity);
+        }
+
+        [Test]
+        [TestCase(-5)]
+        [TestCase(0)]
+        [TestCase(-99)]
+        public void FuelToRefuel_ThrowException_IsInvalid(double fuelRefuel)
+        {
+            CarManager.Car car = new CarManager.Car(make, model, fuelConsumption, fuelCapacity);
+
+            Assert.Throws<ArgumentException>(() => car.Refuel(fuelRefuel));
+        }
+
+        [Test]
+        public void FuelToRefuel_Is_Valid()
+        {
+            CarManager.Car car = new CarManager.Car(make, model, fuelConsumption, fuelCapacity);
+
+            car.Refuel(55);
+
+            double expectedCarFuelAmount = car.FuelAmount;
+
+            Assert.AreEqual(expectedCarFuelAmount, 55);
+        }
+
+        [Test]
+        public void Drive_A_Car_IsValid()
+        {
+            CarManager.Car car = new CarManager.Car(make, model, fuelConsumption, fuelCapacity);
+            car.Refuel(100);
+
+            double fuelNeeded = (50 / 100) * fuelConsumption;
+
+            car.Drive(50);
+
+            double expectedResult = car.FuelAmount - fuelNeeded;
+
+
+            Assert.AreEqual(expectedResult, car.FuelAmount);
+        }
+
+        [Test]
+        public void Check_If_Trunk_Is_Empty_ThrowException()
+        {
+            CarManager.Car car = new CarManager.Car(make, model, fuelConsumption, fuelCapacity);
+
+            Assert.Throws<InvalidOperationException>(() => car.Drive(10));
+
+        }
+
+        [Test]
+        public void Check_If_DecreaseFuelAmount_If_DriveIsValid()
+        {
+            CarManager.Car car = new CarManager.Car(make, model, fuelConsumption, fuelCapacity);
+
+            car.Refuel(car.FuelCapacity);
+
+            car.Drive(100);
+
+            Assert.That(car.FuelAmount, Is.LessThan(car.FuelCapacity));
+        }
+
+        [Test]
+        [TestCase(150)]
+        public void Test_Refuel_IfAmountIsBigger_ThanCapacity(double fuelToRefuel)
+        {
+            CarManager.Car car = new CarManager.Car(make, model, fuelConsumption, fuelCapacity);
+
+            car.Refuel(fuelToRefuel);
+
+            double expectedAmount = car.FuelCapacity;
+
+            Assert.AreEqual(expectedAmount, car.FuelAmount);
         }
     }
 }
--- C# OOP/UnitTesting-Exercises/FightingArena.Tests/WarriorTests.cs
+++ C# OOP/UnitTesting-Exercises/FightingArena.Tests/WarriorTests.cs
@@ -1,18 +1,175 @@
 using NUnit.Framework;
+using FightingArena;
+using System;
 
 namespace Tests
 {
     public class WarriorTests
     {
+        private string name;
+        private int damage;
+        private int hp;
+
         [SetUp]
         public void Setup()
         {
+            name = "name";
+            damage = 10;
+            hp = 50;
         }
 
         [Test]
-        public void Test1()
+        [TestCase("", 20, 50)]
+        [TestCase(null, 20, 50)]
+        [TestCase("name", 0, 50)]
+        [TestCase("name", -15, 50)]
+        [TestCase("name", 15, -20)]
+        public void Ctor_Test_IfThrow_Exception(string name, int damage, int hp)
         {
-            Assert.Pass();
+            Assert.Throws<ArgumentException>(() => new Warrior(name, damage, hp));
+        }
+
+        [Test]
+        public void Check_If_ValueReturnName()
+        {
+            Warrior warrior = new Warrior(name, damage, hp);
+
+            string expectedName = name;
+
+            Assert.AreEqual(expectedName, warrior.Name);
+        }
+
+        [Test]
+        public void Check_If_ValueReturnDamage()
+        {
+            Warrior warrior = new Warrior(name, damage, hp);
+
+            int expectedDamage = damage;
+
+            Assert.AreEqual(expectedDamage, warrior.Damage);
+        }
+
+        [Test]
+        public void Check_If_ValueReturnHp()
+        {
+            Warrior warrior = new Warrior(name, damage, hp);
+
+            int expectedHp = hp;
+
+            Assert.AreEqual(expectedHp, warrior.HP);
+        }
+
+        [Test]
+        [TestCase(15, 30)]
+        [TestCase(35, 25)]
+        [TestCase(40, 30)]
+        public void Check_If_WarriorAttack_Throw_Exception(int hpAttacker, int hpWarrior)
+        {
+            Warrior attacker = new Warrior("Attacker", 20, hpAttacker);
+            Warrior warrior = new Warrior("Warrior", 50, hpWarrior);
+
+            Assert.Throws<InvalidOperationException>(() => attacker.Attack(warrior));
+        }
+
+        [Test]
+        public void Check_If_WarriorHp_SmallerThan_MinAttackHp_Throw_Exception()
+        {
+            Warrior attacker = new Warrior("Attacker", 20, 50);
+            Warrior warrior = new Warrior("Warrior", 60, 15);
+
+            Assert.Throws<InvalidOperationException>(() => attacker.Attack(warrior));
+        }
+
+        [Test]
+        public void Check_If_AttackerHp_SmallerThan_MinAttackHp_Throw_Exception()
+        {
+            Warrior attacker = new Warrior("Attacker", 20, 15);
+            Warrior warrior = new Warrior("Warrior", 60, 55);
+
+            Assert.Throws<InvalidOperationException>(() => attacker.Attack(warrior));
+        }
+
+        [Test]
+        public void If_AttackerHP_Is_Smaller_Than_WarriroDamage_Throw_Exception()
+        {
+            Warrior attacker = new Warrior("Attacker", 20, 50);
+            Warrior warrior = new Warrior("Warrior", 60, 55);
+
+            Assert.Throws<InvalidOperationException>(() => attacker.Attack(warrior));
+        }
+
+        [Test]
+        [TestCase(50, 40)]
+        [TestCase(90, 60)]
+        [TestCase(77, 55)]
+        public void Check_If_AttackerHpDecrease_WithWarriorDamagePoints(int hpAttacker, int warriorDamage)
+        {
+            Warrior attacker = new Warrior("Attacker", 20, hpAttacker);
+            Warrior warrior = new Warrior("Warrior", warriorDamage, 35);
+
+            //attacker.Attack(warrior);
+
+            //Assert.That(attacker.HP, Is.LessThan(hpAttacker));
+
+            int expectedAttackerDecreaseHp = hpAttacker - warriorDamage;
+            int currentHp = attacker.HP - warrior.Damage;
+
+            Assert.AreEqual(expectedAttackerDecreaseHp, currentHp);
+        }
+
+        [Test]
+        [TestCase(50, 45)]
+        [TestCase(70, 50)]
+        public void Check_If_WarriorXp_IfIsSmaller_ThanAttackerDamage_AfterAttackIsZero(int attackerDamage, int warriorHp)
+        {
+            Warrior attacker = new Warrior("Attacker", attackerDamage, 40);
+            Warrior warrior = new Warrior("Warrior", 40, warriorHp);
+
+            attacker.Attack(warrior);
+
+            int expectedResult = 0;
+            int currResult = warrior.HP;
+
+            Assert.AreEqual(expectedResult, currResult);
+        }
+
+        [Test]
+        [TestCase(45, 50)]
+        [TestCase(60, 70)]
+        public void Check_If_WarriorXp_IsDecrease_ByAttackerHpPoints(int attackerDamage, int warriorHp)
+        {
+            Warrior attacker = new Warrior("Attacker", attackerDamage, 40);
+            Warrior warrior = new Warrior("Warrior", 40, warriorHp);
+
+            attacker.Attack(warrior);
+
+            int expectedResult = warriorHp - attackerDamage;
+            int currResult = warrior.HP;
+
+            Assert.AreEqual(expectedResult, currResult);
+        }
+
+        [Test]
+        public void CheckIf_AttackerHp_DecreaseWith_WarriorDamagePoints()
+        {
+            Warrior attacker = new Warrior("Attacker", 100, 50);
+            Warrior warrior = new Warrior("Warrior", 40, 150);
+
+            int expectedResult = 10;
+            int currentResult = attacker.HP - warrior.Damage;
+
+            warrior.Attack(attacker);
+
+            Assert.AreEqual(expectedResult, currentResult);
+        }
+
+        [Test]
+        public void Check_If_AttackChangeHPToZero()
+        {
+            Warrior attacker = new Warrior("pesho", 50, 55);
+            Warrior warrior = new Warrior("warrior", 50, 45);
+            attacker.Attack(warrior);
+            Assert.That(warrior.HP, Is.EqualTo(0));
         }
     }
 }
