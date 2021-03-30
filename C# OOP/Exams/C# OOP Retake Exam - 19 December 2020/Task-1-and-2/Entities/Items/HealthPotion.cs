63072ee1cc6e7bdb89fc6c55c61a1d6d078b0860
Plamen P <pit_sofia@hotmail.com>
2021-03-30 15:14:36 +0300

    Past Exams Upload
    Signed-off-by: Plamen P. <pit_sofia@hotmail.com>

---
+++ C# OOP/Exams/C# OOP Retake Exam - 19 December 2020/Task-1-and-2/Entities/Items/HealthPotion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WarCroft.Entities.Characters.Contracts;
+
+namespace WarCroft.Entities.Items
+{
+    public class HealthPotion
+    {
+        private const int Weight = 5;
+
+        public HealthPotion(int health)
+        {
+            Health = health;
+        }
+
+        public int Health { get; private set; }
+
+        void AffectCharacter(Character character)
+        {
+            if (character.IsAlive)
+            {
+                Health += 20;
+            }
+        }
+    }
+}
