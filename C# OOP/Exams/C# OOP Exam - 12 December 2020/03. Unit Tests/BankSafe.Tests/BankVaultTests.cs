6165f3bd75dffd55c914fc3f4edc26c913935e66
Plamen P <pit_sofia@hotmail.com>
2021-04-01 13:18:56 +0300

    Past Exams
    Signed-off-by: Plamen P. <pit_sofia@hotmail.com>

--- C# OOP/Exams/C# OOP Exam - 12 December 2020/03. Unit Tests/BankSafe.Tests/BankVaultTests.cs
+++ C# OOP/Exams/C# OOP Exam - 12 December 2020/03. Unit Tests/BankSafe.Tests/BankVaultTests.cs
@@ -1,19 +1,81 @@
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 
 namespace BankSafe.Tests
 {
     public class BankVaultTests
     {
+        BankVault collection = new BankVault();
+
+        Item item = new Item("aaa", "bbb");
+
+
         [SetUp]
         public void Setup()
         {
+            // collection = new BankVault()
         }
 
         [Test]
-        public void Test()
+        public void ThrowException_If_TryToAddItemInCollection_If_CellNotExist()
+        {
+            Assert.Throws<ArgumentException>(() => collection.AddItem("a1", item));
+        }
+        [Test]
+        public void ThrowException_If_CellIsAllreadyTaken()
+        {
+            collection.AddItem("A1", item);
+
+            Assert.Throws<ArgumentException>(() => collection.AddItem("A1", new Item("ccc", "xxx")));
+        }
+
+        [Test]
+        public void CheckIf_CellExist_ThrowException()
         {
 
+           // collection.AddItem("A1", item);
+
+            Assert.Throws<InvalidOperationException>(() => collection.AddItem("A2", item));
+        }
+
+        [Test]
+        public void AddItemInCollection()
+        {
+            string expectedResult = collection.AddItem("A1", item);
+
+            Assert.AreEqual(expectedResult, $"Item:{item.ItemId} saved successfully!");
+        }
+
+        [Test]
+        public void RemoveItem_ThrowException_WhenItemNotExist()
+        {
+            //collection.AddItem("A1", item);
+
+            Assert.Throws<ArgumentException>(() => collection.RemoveItem("A1", new Item("ccc", "xxx")));
+        }
+
+        [Test]
+        public void RemoveItem_ThrowException_WhenCellNotExist()
+        {
+            Assert.Throws<ArgumentException>(() => collection.RemoveItem("A", item));
+        }
+
+        [Test]
+        public void RemoItem_Successffuly()
+        {
+           // collection.AddItem("A1", item);
+
+            string expectedResult = collection.RemoveItem("A1", item);
+
+            Assert.AreEqual(expectedResult, $"Remove item:{item.ItemId} successfully!");
+        }
+
+        [Test]
+        public void TestDictionaryCount()
+        {
+            
+            Assert.That(collection.VaultCells.Count, Is.EqualTo(12));
         }
     }
 }
---
+++ C# OOP/Exams/C# OOP Exam - 12 December 2020/Task-1-and-2/Bakery/Models/Tables/Table.cs
@@ -0,0 +1,117 @@
+﻿using Bakery.Models.BakedFoods.Contracts;
+using Bakery.Models.Drinks.Contracts;
+using Bakery.Models.Tables.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bakery.Models.Tables
+{
+    public abstract class Table : ITable
+    {
+        private readonly List<IBakedFood> foodOrders;
+        private readonly List<IDrink> drinkOrders;
+        private int capacity;
+        private int numberOfPeople;
+
+        public Table(int tableNumber, int capacity, decimal pricePerPerson)
+        {
+            TableNumber = tableNumber;
+            Capacity = capacity;
+            PricePerPerson = pricePerPerson;
+
+            foodOrders = new List<IBakedFood>();
+            drinkOrders = new List<IDrink>();
+
+        }
+
+        private IReadOnlyCollection<IBakedFood> FoodOrders
+           => foodOrders.AsReadOnly();
+
+        private IReadOnlyCollection<IDrink> DrinkOrders
+            => drinkOrders.AsReadOnly();
+
+        public int TableNumber { get; private set; }
+
+        public int Capacity
+        {
+            get => capacity;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Capacity has to be greater than 0");
+                }
+
+                capacity = value;
+            }
+        }
+
+        public int NumberOfPeople
+        {
+            get => numberOfPeople;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Cannot place zero or less people!");
+                }
+
+                numberOfPeople = value;
+            }
+        }
+
+        public decimal PricePerPerson { get; private set; }
+
+        public bool IsReserved { get; private set; }
+
+        public decimal Price => foodOrders.Select(x => x.Price).Sum()
+                                + drinkOrders.Select(x => x.Price).Sum()
+                                + NumberOfPeople * PricePerPerson;
+
+        public void Clear()
+        {
+            NumberOfPeople = 0;
+            drinkOrders.Clear();
+            foodOrders.Clear();
+            //IsReserved = false;
+            //Capacity = 0;
+            
+        }
+
+        public decimal GetBill()
+        {
+            return Price;
+        }
+
+        public string GetFreeTableInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Table: {TableNumber}");
+            sb.AppendLine($"Type: {GetType().Name}");
+            sb.AppendLine($"Capacity: {Capacity}");
+            sb.AppendLine($"Price per Person: {PricePerPerson}");
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public void OrderDrink(IDrink drink)
+        {
+            drinkOrders.Add(drink);
+        }
+
+        public void OrderFood(IBakedFood food)
+        {
+            foodOrders.Add(food);
+        }
+
+        public void Reserve(int numberOfPeople)
+        {
+            IsReserved = true;
+            NumberOfPeople = numberOfPeople;
+
+        }
+    }
+}
