5b288b2bf3a58f11e06b40686caec7749e27aae3
Plamen P <pit_sofia@hotmail.com>
2021-04-02 20:04:15 +0300

    Past Exam
    Signed-off-by: Plamen P. <pit_sofia@hotmail.com>

---
+++ C# OOP/Exams/C# OOP Retake Exam - 22 August 2020/Task-1-and-2/Exam-Skeleton/EasterRaces/Core/Entities/ChampionshipController.cs
@@ -0,0 +1,40 @@
+﻿using EasterRaces.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasterRaces.Core.Entities
+{
+    public class ChampionshipController : IChampionshipController
+    {
+        public string AddCarToDriver(string driverName, string carModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string AddDriverToRace(string raceName, string driverName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CreateCar(string type, string model, int horsePower)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CreateDriver(string driverName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CreateRace(string name, int laps)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string StartRace(string raceName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ C# OOP/Exams/C# OOP Retake Exam - 22 August 2020/Task-1-and-2/Exam-Skeleton/EasterRaces/Models/Drivers/Entities/Driver.cs
@@ -0,0 +1,53 @@
+﻿using EasterRaces.Models.Cars.Contracts;
+using EasterRaces.Models.Drivers.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasterRaces.Models.Drivers.Entities
+{
+    public class Driver : IDriver        // ????????? abstract
+    {
+        private string name;
+
+        public Driver(string name)
+        {
+            Name = name;
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value) && value.Length < 5)  // ??? value.Lenght <= 5
+                {
+                    throw new ArgumentException($"Name {value} cannot be less than 5 symbols.");
+                }
+
+                name = value;
+            }
+        }
+
+        public ICar Car { get; private set; }
+
+        public int NumberOfWins { get; private set; }
+
+        public bool CanParticipate => Car != null;
+
+        public void AddCar(ICar car)
+        {
+            if (car == null)
+            {
+                throw new ArgumentNullException("Car cannot be null.");
+            }
+
+            Car = car;
+        }
+
+        public void WinRace()
+        {
+            NumberOfWins++;
+        }
+    }
+}
