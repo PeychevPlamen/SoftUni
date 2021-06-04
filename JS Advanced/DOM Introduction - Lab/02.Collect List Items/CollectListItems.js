a5433ec1a7eb7fabb5a0855b5eb19f4d90c8f567
Plamen P <pit_sofia@hotmail.com>
2021-06-04 12:52:51 +0300

    JavaScript Advanced - Lab uploads
    Signed-off-by: Plamen P. <pit_sofia@hotmail.com>

--- JS Advanced/DOM Introduction - Lab/02.Collect List Items/CollectListItems.js
+++ JS Advanced/DOM Introduction - Lab/02.Collect List Items/CollectListItems.js
@@ -2,5 +2,5 @@ function extractText() {
     const elements = [...document.getElementsByTagName('li')];
     const elementText = [...elements].map(x => x.textContent)
     const inputArea = document.getElementById('result');
-   inputArea.value = elementText.join('\n');
+    inputArea.value = elementText.join('\n');
 }
