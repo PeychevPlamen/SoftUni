dbc512523f4291bcabcd37f475872ec1db5b44d3
Plamen P <pit_sofia@hotmail.com>
2021-06-06 17:25:10 +0300

    JavaScript Advanced - Exercise uploads
    Signed-off-by: Plamen P. <pit_sofia@hotmail.com>

--- JS Advanced/DOM Introduction - Exercise/04. Search in List/search.js
+++ JS Advanced/DOM Introduction - Exercise/04. Search in List/search.js
@@ -1,3 +1,25 @@
 function search() {
-   // TODO
+
+   let towns = document.querySelectorAll('#towns li');
+   let searchInput = document.getElementById('searchText').value;
+   let result = document.getElementById('result');
+
+   let sum = 0;
+
+   for (const town of towns) {
+
+      if (town.textContent.includes(searchInput)) {
+
+         town.style.fontWeight = 'bold';
+         town.style.textDecoration = 'underline';
+         sum++;
+      }else{
+         town.style.fontWeight = '';
+         town.style.textDecoration = '';
+      }
+
+   }
+   document.getElementById('searchText').value = '';
+   result.textContent = `${sum} matches found`;
+
 }
--- JS Advanced/DOM Introduction - Exercise/05. Table - Serach Engine/solution.js
+++ JS Advanced/DOM Introduction - Exercise/05. Table - Serach Engine/solution.js
@@ -2,7 +2,21 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
 
+      let tableRows = document.querySelectorAll('tbody tr');
+      let inputField = document.getElementById('searchField').value;
+      //let button = document.querySelector('#searchBtn');
+      
+      for (const row of tableRows) {
+
+         if (row.textContent.includes(inputField)) {
+
+            row.classList.add('select');
+            
+         } else {
+            row.removeAttribute('class');
+         }
+      }
+      document.getElementById('searchField').value = '';
    }
 }
