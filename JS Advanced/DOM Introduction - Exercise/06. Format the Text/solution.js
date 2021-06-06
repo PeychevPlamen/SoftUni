5ecb523a3532ea93bdadfc8270e3d1d835c94ded
Plamen P <pit_sofia@hotmail.com>
2021-06-06 19:00:52 +0300

    JavaScript Advanced - Exercise uploads
    Signed-off-by: Plamen P. <pit_sofia@hotmail.com>

--- JS Advanced/DOM Introduction - Exercise/06. Format the Text/solution.js
+++ JS Advanced/DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-  //TODO
+
+  const textArea = document.getElementById('input').value.split('.').filter(e => e.length >= 1);
+  //const button = document.getElementById('formatItBtn');
+  const output = document.getElementById('output');
+
+  let result = [];
+
+  while (textArea.length > 0) {
+
+    result.push(textArea.splice(0, 3).join('.') + '.');
+
+  }
+
+  result.forEach(p => (output.innerHTML += `<p>${p}</p>`));
 }
