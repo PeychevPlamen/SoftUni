6cf28683d93153ba0406a18674713566c81d8fba
Plamen P <pit_sofia@hotmail.com>
2021-06-19 17:02:34 +0300

    JavaScript Advanced - Exercise uploads
    Signed-off-by: Plamen P. <pit_sofia@hotmail.com>

---
+++ JS Advanced/Advanced Functions - Exercise/02.Argument-Info.js
@@ -0,0 +1,29 @@
+function solve(...input) {
+
+    const types = [];
+    const result = [];
+
+    input.forEach(x => {
+        types.push({
+            [typeof x]: x
+        });
+
+        result.push(`${typeof x}: ${x}`);
+    });
+
+    const counts = {};
+
+    types.forEach(t => {
+        let key = Object.keys(t)[0];
+
+        !counts[key] ? counts[key] = 1 : counts[key]++;
+    });
+
+    Object.keys(counts)
+        .sort((a, b) => counts[b] - counts[a])
+        .forEach(k => result.push(`${k} = ${counts[k]}`));
+
+    result.forEach(x => console.log(x));
+
+}
+console.log(solve('cat', 42, function () { console.log('Hello world!'); }));
