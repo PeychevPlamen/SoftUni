01e4e96397dd3bcdc72b8137e96f5be8669f9f79
Plamen P <pit_sofia@hotmail.com>
2021-09-23 20:49:28 +0300

    MS SQL - Exercise uploads
    Signed-off-by: Plamen P. <pit_sofia@hotmail.com>

--- MS SQL/Table Relations/TableRelationsEx.sql
+++ MS SQL/Table Relations/TableRelationsEx.sql
@@ -64,3 +64,141 @@ INSERT INTO [Models] ([Name], [ManufacturerID])
 
 --		Problem 3.	Many-To-Many Relationship
 
+CREATE TABLE [Students]
+	(
+	[StudentID] INT PRIMARY KEY IDENTITY NOT NULL,
+	[Name] VARCHAR(50) NOT NULL
+	)
+
+CREATE TABLE [Exams]
+	(
+	[ExamID] INT PRIMARY KEY IDENTITY(101, 1) NOT NULL,
+	[Name] VARCHAR(50) NOT NULL
+	)
+
+CREATE TABLE [StudentsExams]
+	(
+	[StudentID] INT FOREIGN KEY REFERENCES [Students]([StudentID]) NOT NULL,
+	[ExamID] INT FOREIGN KEY REFERENCES [Exams]([ExamID]) NOT NULL,
+	PRIMARY KEY ([StudentID], [ExamID])
+	)
+
+--		Problem 4.	Self-Referencing 
+
+CREATE TABLE [Teachers]
+	(
+	[TeacherID] INT PRIMARY KEY IDENTITY (101, 1) NOT NULL,
+	[Name] VARCHAR(50) NOT NULL,
+	[ManagerID] INT FOREIGN KEY REFERENCES [Teachers]([TeacherID])
+	)
+
+INSERT INTO [Teachers] ([Name], [ManagerID])
+	 VALUES
+		('John', NULL)
+		,('Maya', 106)
+		,('Silvia', 106)
+		,('Ted', 105)
+		,('Mark', 101)
+		,('Greta', 101)
+
+
+--		Problem 5.	Online Store Database
+
+CREATE DATABASE [OnlineStore]
+
+USE [OnlineStore]
+
+CREATE TABLE [Cities] 
+	(
+	[CityID] INT PRIMARY KEY IDENTITY NOT NULL,
+	[Name] VARCHAR(50) NOT NULL
+	)
+
+CREATE TABLE [Customers]
+	(
+	[CustomerID] INT PRIMARY KEY IDENTITY NOT NULL
+	,[Name] VARCHAR(50) NOT NULL
+	,[Birthday] DATE
+	,[CityID] INT FOREIGN KEY REFERENCES [Cities]([CityID])
+	)
+
+CREATE TABLE [ItemTypes]
+	(
+	[ItemTypeID] INT PRIMARY KEY IDENTITY NOT NULL
+	,[Name] VARCHAR(50)
+	)
+
+CREATE TABLE [Items]
+	(
+	[ItemID] INT PRIMARY KEY IDENTITY NOT NULL
+	,[Name] VARCHAR(50)
+	,[ItemTypeID] INT FOREIGN KEY REFERENCES [ItemTypes]([ItemTypeID])
+	)
+
+CREATE TABLE [Orders]
+	(
+	[OrderID] INT PRIMARY KEY IDENTITY NOT NULL
+	,[CustomerID] INT NOT NULL
+	)
+
+ALTER TABLE [Orders]
+ADD CONSTRAINT FK_CustomersOrders
+FOREIGN KEY ([CustomerID]) REFERENCES [Customers]([CustomerID])
+
+CREATE TABLE [OrderItems]
+	(
+	[OrderID] INT FOREIGN KEY REFERENCES [Orders]([OrderID]) NOT NULL
+	,[ItemID] INT FOREIGN KEY REFERENCES [Items]([ItemID]) NOT NULL
+	, PRIMARY KEY ([OrderID], [ItemID])
+	)
+	 
+--		Problem 6.	University Database
+
+CREATE DATABASE [University]
+
+USE [University]
+
+CREATE TABLE [Subjects]
+	(
+	[SubjectID] INT PRIMARY KEY IDENTITY NOT NULL
+	,[SubjectName] VARCHAR(50) NOT NULL
+	)
+
+CREATE TABLE [Majors]
+	(
+	[MajorID] INT PRIMARY KEY IDENTITY NOT NULL
+	,[Name] VARCHAR(50) NOT NULL
+	)
+
+CREATE TABLE [Students]
+	(
+	[StudentID] INT PRIMARY KEY IDENTITY NOT NULL
+	,[StudentNumber] INT NOT NULL
+	,[StudentName] VARCHAR(50)
+	,[MajorID] INT FOREIGN KEY REFERENCES [Majors]([MajorID])
+	)
+
+CREATE TABLE [Payments]
+	(
+	[PaymentID] INT PRIMARY KEY IDENTITY NOT NULL
+	,[PaymentDate] DATE
+	,[PaymentAmount] DECIMAL(9, 2)
+	,[StudentID] INT FOREIGN KEY REFERENCES [Students]([StudentID])
+	)
+
+CREATE TABLE [Agenda]
+	(
+	[StudentID] INT FOREIGN KEY REFERENCES [Students]([StudentID]) NOT NULL
+	,[SubjectID] INT FOREIGN KEY REFERENCES [Subjects]([SubjectID]) NOT NULL
+	, PRIMARY KEY ([StudentID], [SubjectID])
+	)
+
+--		Problem 9.	*Peaks in Rila
+
+USE [Geography]
+
+SELECT m.MountainRange, p.PeakName, p.Elevation
+FROM [Mountains] AS m
+JOIN PEAKS AS p ON p.MountainId = m.Id
+WHERE m.MountainRange = 'Rila'
+ORDER BY p.Elevation DESC
