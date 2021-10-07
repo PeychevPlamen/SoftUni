67a2933b093d0770dc45ce3cba3df5d9990eae26
Plamen P <pit_sofia@hotmail.com>
2021-10-07 23:04:26 +0300

    MS SQL - Exercise uploads
    Signed-off-by: Plamen P. <pit_sofia@hotmail.com>

--- MS SQL/Subqueries and Joins/Subqueries-and-Joins.sql
+++ MS SQL/Subqueries and Joins/Subqueries-and-Joins.sql
@@ -169,3 +169,23 @@ ORDER BY e.DepartmentID
    GROUP BY c.CountryName
    ORDER BY HighestPeakElevation DESC, LongestRiverLength DESC, c.CountryName
    
+	--	18. Highest Peak Name and Elevation by Country
+
+	SELECT TOP (5) CountryName AS Country
+		   , ISNULL(PeakName, '(no highest peak)') AS [Highest Peak Name]
+		   , ISNULL(Elevation, 0) AS [Highest Peak Elevation]
+		   , ISNULL(MountainRange, '(no mountain)') AS Mountain
+	  FROM
+	(
+	SELECT c.CountryName
+		 , p.PeakName
+		 , p.Elevation
+		 , m.MountainRange
+		 , DENSE_RANK() OVER(PARTITION BY c.CountryCode ORDER BY p.Elevation DESC) AS PeakRank
+	  FROM Countries AS c
+	   LEFT JOIN MountainsCountries AS mc ON c.CountryCode = mc.CountryCode
+	   LEFT JOIN Mountains AS m ON mc.MountainId = m.Id
+	   LEFT JOIN Peaks AS p ON m.Id = p.MountainId
+	  ) AS PeaksRankingSubQuery
+	 WHERE PeakRank = 1
+  ORDER BY CountryName, [Highest Peak Name]  
