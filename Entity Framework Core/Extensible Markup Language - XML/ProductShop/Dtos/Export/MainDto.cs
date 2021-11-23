ee3b01d33dc5a5b1958abb043e4e4be0c49a93bc
Plamen P <pit_sofia@hotmail.com>
2021-11-23 15:30:23 +0200

    XML Processing - Exercise
    Signed-off-by: Plamen P. <pit_sofia@hotmail.com>

---
+++ Entity Framework Core/Extensible Markup Language - XML/ProductShop/Dtos/Export/MainDto.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections.Generic;
+
+using System.Xml.Serialization;
+
+namespace ProductShop.Dtos.Export
+{
+    [XmlType("Users")]
+    public class MainDto
+    {
+        [XmlAttribute("Count")]
+        public int Count { get; set; }
+
+        [XmlElement("users")]
+        public UserSoldProductsDto[] Users { get; set; }
+    }
+}
