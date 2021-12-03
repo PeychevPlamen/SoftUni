605965fc70d35cbbd0dd5252346b76f231f55a94
Plamen P <pit_sofia@hotmail.com>
2021-12-03 21:21:31 +0200

    Exam - 08 August 2020
    Signed-off-by: Plamen P. <pit_sofia@hotmail.com>

---
+++ Entity Framework Core/C# DB Advanced Exam - 08 August 2020/VaporStore/DataProcessor/Serializer.cs
@@ -0,0 +1,48 @@
+﻿namespace VaporStore.DataProcessor
+{
+    using System;
+    using System.Linq;
+    using Data;
+    using Newtonsoft.Json;
+
+    public static class Serializer
+    {
+        public static string ExportGamesByGenres(VaporStoreDbContext context, string[] genreNames)
+        {
+            var genres = context.Genres
+                .Where(x => genreNames.Contains(x.Name))
+                .ToArray()
+                .Select(x => new
+                {
+                    Id = x.Id,
+                    Genre = x.Name,
+                    Games = x.Games.Where(g => g.Purchases.Any())
+                    .ToArray()
+                        .Select(g => new
+                        {
+                            Id = g.Id,
+                            Title = g.Name,
+                            Developer = g.Developer.Name,
+                            Tags = string.Join(", ", g.GameTags.Select(gt => gt.Tag.Name)),
+                            Players = g.Purchases.Count
+                        })
+                        .OrderByDescending(g => g.Players)
+                        .ThenBy(g => g.Id)
+                        .ToArray(),
+                    TotalPlayers = x.Games.Select(z=>z.Purchases.Count).Sum()
+
+                })
+                .OrderByDescending(x=>x.TotalPlayers)
+                .ThenBy(x=>x.Id)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(genres, Formatting.Indented);
+
+        }
+
+        public static string ExportUserPurchasesByType(VaporStoreDbContext context, string storeType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
