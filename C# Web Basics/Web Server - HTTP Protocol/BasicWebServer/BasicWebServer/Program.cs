f28a30a529205524a959dc47bbcdc40e81c9a5db
Plamen P <pit_sofia@hotmail.com>
2022-01-15 18:26:17 +0200

    C#Web-Server-HTTP Protocol
    Signed-off-by: Plamen P. <pit_sofia@hotmail.com>

---
+++ C# Web Basics/Web Server - HTTP Protocol/BasicWebServer/BasicWebServer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace BasicWebServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ipAddress = IPAddress.Parse("127.0.0.1");
+            var port = 8080;
+
+            var serverListener = new TcpListener(ipAddress, port);
+            serverListener.Start();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
