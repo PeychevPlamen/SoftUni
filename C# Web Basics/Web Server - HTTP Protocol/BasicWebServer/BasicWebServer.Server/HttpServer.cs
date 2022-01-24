a85be463937db46fa652d7c8b999b74cd554de65
Plamen P <pit_sofia@hotmail.com>
2022-01-24 15:43:17 +0200

    C# Basic WEB Server Async
    Signed-off-by: Plamen P. <pit_sofia@hotmail.com>

--- C# Web Basics/Web Server - HTTP Protocol/BasicWebServer/BasicWebServer.Server/HttpServer.cs
+++ C# Web Basics/Web Server - HTTP Protocol/BasicWebServer/BasicWebServer.Server/HttpServer.cs
@@ -38,7 +38,7 @@ namespace BasicWebServer.Server
         {
 
         }
-        public void Start()
+        public async Task Start()
         {
             this.serverListener.Start();
 
@@ -47,37 +47,41 @@ namespace BasicWebServer.Server
 
             while (true)
             {
-                var connection = serverListener.AcceptTcpClient();
+                var connection = await serverListener.AcceptTcpClientAsync();
 
-                var networkStream = connection.GetStream();
+                _ = Task.Run(async () =>
+                {
+                    var networkStream = connection.GetStream();
 
-                var requestText = this.ReadRequest(networkStream);
+                    var requestText = await this.ReadRequest(networkStream);
 
-                Console.WriteLine(requestText);
+                    Console.WriteLine(requestText);
 
-                var request = Request.Parse(requestText);
+                    var request = Request.Parse(requestText);
 
-                var response = this.rountingTable.MatchRequest(request);
+                    var response = this.rountingTable.MatchRequest(request);
 
-                // Execute pre-render action for the response
-                if (response.PreRenderAction != null)
-                    response.PreRenderAction(request, response);
+                    // Execute pre-render action for the response
+                    if (response.PreRenderAction != null)
+                        response.PreRenderAction(request, response);
 
-                WriteResponse(networkStream, response);
+                    await WriteResponse(networkStream, response);
 
-                connection.Close();
+                    connection.Close();
+                });
+
             }
         }
 
-        private void WriteResponse(NetworkStream networkStream, Response response)
+        private async Task WriteResponse(NetworkStream networkStream, Response response)
         {
 
             var responseBytes = Encoding.UTF8.GetBytes(response.ToString());
 
-            networkStream.Write(responseBytes);
+            await networkStream.WriteAsync(responseBytes);
         }
 
-        private string ReadRequest(NetworkStream networkStream)
+        private async Task<string> ReadRequest(NetworkStream networkStream)
         {
             var bufferLength = 1024;
             var buffer = new byte[bufferLength];
@@ -88,7 +92,7 @@ namespace BasicWebServer.Server
 
             do
             {
-                var bytesRead = networkStream.Read(buffer, 0, bufferLength);
+                var bytesRead = await networkStream.ReadAsync(buffer, 0, bufferLength);
 
                 totalBytes += bytesRead;
 
