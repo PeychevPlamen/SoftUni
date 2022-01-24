a353b6d3f6c80621b979bf989314c8c122d2d127
Plamen P <pit_sofia@hotmail.com>
2022-01-24 16:53:23 +0200

    C# Basic Web Server async
    Signed-off-by: Plamen P. <pit_sofia@hotmail.com>

--- C# Web Basics/Web Server - HTTP Protocol/BasicWebServer/BasicWebServer.Demo/StartUp.cs
+++ C# Web Basics/Web Server - HTTP Protocol/BasicWebServer/BasicWebServer.Demo/StartUp.cs
@@ -7,24 +7,63 @@ namespace BasicWebServer.Demo
 {
     public class StartUp
     {
-        public static void Main()
-            => new HttpServer(routes => routes
-            .MapGet("/", new TextResponse("Hello from the server!"))
-            //.MapGet("/HTML", new HtmlResponse("<h1>HTML response</h1>"))
-            .MapGet("/Redirect", new RedirectResponse("http://softuni.org/"))
-            .MapGet("/HTML", new HtmlResponse(StartUp.HtmlForm))
-            .MapPost("/HTML", new TextResponse("", StartUp.AddFormDataAction)))
-            .Start();
-
-        //var server = new HttpServer("127.0.0.1", 8080);
-        //server.Start();
-
-        private const string HtmlForm = 
+        private const string HtmlForm =
             @"<form action='/HTML' method='POST'>
             Name: <input type ='text' name ='Name'/>
             Age: <input type ='number' name= 'Age'/>
             <input type = 'submit' value = 'Save'/>
             </form>";
+
+        private const string DownloadForm =
+            @"<form action='/Content' method='POST'>
+            <input type='submit' value = 'Download Sites Content' />
+            </form>";
+
+        private const string FileName = "content.txt";
+        public static async Task Main()
+        {
+            await DownloadSitesAsTextFile(FileName, new string[] {
+            "https://judge.softuni.org/", "https://softuni.org"
+            });
+
+            var server = new HttpServer(routes => routes
+            .MapGet("/", new TextResponse("Hello from the server!"))
+            .MapGet("/Redirect", new RedirectResponse("http://softuni.org/"))
+            .MapGet("/HTML", new HtmlResponse(StartUp.HtmlForm))
+            .MapPost("/HTML", new TextResponse("", StartUp.AddFormDataAction))
+            .MapGet("/Content", new HtmlResponse(DownloadForm))
+            .MapPost("/Content", new TextFileResponse(FileName)));
+
+            await server.Start();
+        }
+
+        private static async Task<string> DownloadWebSiteContent(string url)
+        {
+            var httpClient = new HttpClient();
+            using (httpClient)
+            {
+                var response = await httpClient.GetAsync(url);
+                var html = await response.Content.ReadAsStringAsync();
+
+                return html.Substring(0, 2000);
+            }
+        }
+
+        private static async Task DownloadSitesAsTextFile(string fileName, string[] urls)
+        {
+            var downloads = new List<Task<string>>();
+
+            foreach (var url in urls)
+            {
+                downloads.Add(DownloadWebSiteContent(url));
+            }
+
+            var responses = await Task.WhenAll(downloads);
+
+            var responsesString = string.Join(Environment.NewLine + new String('-', 100), responses);
+
+            await File.WriteAllTextAsync(fileName, responsesString);
+        }
 
         private static void AddFormDataAction(Request request, Response response)
         {
