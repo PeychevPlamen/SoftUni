c52f3ebc1fd209ec12b1d41fcc8fab238e312059
Plamen P <pit_sofia@hotmail.com>
2022-02-01 17:18:22 +0200

    C# Basic WEB Server Views
    Signed-off-by: Plamen P. <pit_sofia@hotmail.com>

--- C# Web Basics/Web Server - HTTP Protocol/BasicWebServer/BasicWebServer.Demo/Controllers/HomeController.cs
+++ C# Web Basics/Web Server - HTTP Protocol/BasicWebServer/BasicWebServer.Demo/Controllers/HomeController.cs
@@ -11,18 +11,6 @@ namespace BasicWebServer.Demo.Controllers
 {
     public class HomeController : Controller
     {
-        private const string HtmlForm =
-            @"<form action='/HTML' method='POST'>
-            Name: <input type ='text' name ='Name'/>
-            Age: <input type ='number' name= 'Age'/>
-            <input type = 'submit' value = 'Save'/>
-            </form>";
-
-        private const string DownloadForm =
-            @"<form action='/Content' method='POST'>
-            <input type='submit' value = 'Download Sites Content' />
-            </form>";
-
         private const string FileName = "content.txt";
 
         public HomeController(Request request)
@@ -33,7 +21,7 @@ namespace BasicWebServer.Demo.Controllers
 
         public Response Index() => Text("Hello from the server");
         public Response Redirect() => Redirect("https://");
-        public Response Html() => Html(HtmlForm);
+        public Response Html() => View();
         public Response HtmlFormPost()
         {
             string formData = string.Empty;
@@ -47,7 +35,7 @@ namespace BasicWebServer.Demo.Controllers
             return Text(formData);
         }
 
-        public Response Content() => Html(DownloadForm);
+        public Response Content() => View();
 
         public Response DownloadContent()
         {
--- C# Web Basics/Web Server - HTTP Protocol/BasicWebServer/BasicWebServer.Demo/Controllers/UsersController.cs
+++ C# Web Basics/Web Server - HTTP Protocol/BasicWebServer/BasicWebServer.Demo/Controllers/UsersController.cs
@@ -10,13 +10,6 @@ namespace BasicWebServer.Demo.Controllers
 {
     public class UsersController : Controller
     {
-        private const string LoginForm =
-            @"<form action='/Login' method='POST'>
-            Username: <input type='text' name='Username'/>
-            Password: <input type='password' name='Password'/>
-            <input type='submit' value ='Log In' /> 
-            </form>";
-
         private const string Username = "user";
         private const string Password = "user123";
 
@@ -25,7 +18,7 @@ namespace BasicWebServer.Demo.Controllers
         {
         }
 
-        public Response Login() => Html(LoginForm);
+        public Response Login() => View();
 
         public Response LogInUser()
         {
