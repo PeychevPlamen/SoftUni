a5a49aa5a052ecb1df682c3e110f551f156235de
Plamen P <pit_sofia@hotmail.com>
2022-02-14 22:28:36 +0200

    C# WEB ExamPrep-CarShop
    Signed-off-by: Plamen P. <pit_sofia@hotmail.com>

---
+++ C# Web Basics/ExamPrep-CarShop/CarShop/CarShop/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace CarShop
+{
+    using BasicWebServer.Server;
+    using BasicWebServer.Server.Routing;
+    using CarShop.Data;
+    using System.Threading.Tasks;
+
+    public class Startup
+    {
+        public static async Task Main()
+        {
+            var server = new HttpServer(routes => routes
+               .MapControllers()
+               .MapStaticFiles());
+
+
+            server.ServiceCollection
+                .Add<ApplicationDbContext>();
+                
+            
+            await server.Start();
+        }
+    }
+}
