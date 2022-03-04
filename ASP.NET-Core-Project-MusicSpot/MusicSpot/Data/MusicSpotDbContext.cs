d191b8045a6caaf30302e5827178d6be7d984524
Plamen P <pit_sofia@hotmail.com>
2022-03-04 17:12:49 +0200

    MusicSpot - My ASP.NET Core Project

---
+++ ASP.NET-Core-Project-MusicSpot/MusicSpot/Data/MusicSpotDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MusicSpot.Data
+{
+    public class MusicSpotDbContext : IdentityDbContext
+    {
+        public MusicSpotDbContext(DbContextOptions<MusicSpotDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
