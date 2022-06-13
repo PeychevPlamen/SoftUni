c45fc5ac6daf6d261f1d50d3d99899e07fa57b42
Plamen P <pit_sofia@hotmail.com>
2022-06-13 15:32:24 +0300

    add some tasks

---
+++ Algorithms Fundamentals with C#/Recursion-and-Backtracking/Recursion-And-Backtracking/Generating-Combinations/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Generating_Combinations
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int k = int.Parse(Console.ReadLine());
+
+            var vector = new int[k];
+
+            Generate(nums, vector, 0, 0);
+        }
+
+        private static void Generate(int[] nums, int[] vector, int index, int border)
+        {
+            if (index == vector.Length)
+            {
+                Console.WriteLine((string.Join(' ', vector)));
+
+            }
+            else
+            {
+                for (int i = border; i < nums.Length; i++)
+                {
+                    vector[index] = nums[i];
+                    Generate(nums, vector, index + 1, i + 1);
+                }
+            }
+        }
+    }
+}
