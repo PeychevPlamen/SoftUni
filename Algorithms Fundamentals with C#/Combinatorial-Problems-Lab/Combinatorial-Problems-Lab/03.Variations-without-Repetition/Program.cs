2bae42cc9c97e7ce38be4a6532d24e3720431c45
Plamen P <pit_sofia@hotmail.com>
2022-06-19 14:40:25 +0300

    add tasks

---
+++ Algorithms Fundamentals with C#/Combinatorial-Problems-Lab/Combinatorial-Problems-Lab/03.Variations-without-Repetition/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _03.Variations_without_Repetition
+{
+    public class Program
+    {
+        private static string[] elements;
+        private static string[] variations;
+        private static bool[] used;
+        private static int k;
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split(' ');
+            k = int.Parse(Console.ReadLine());
+
+            variations=new string[k];
+            used = new bool[elements.Length];
+
+            Variations(0);
+        }
+
+        private static void Variations(int index)
+        {
+            if (index >= variations.Length)
+            {
+                Console.WriteLine(string.Join(" ", variations));
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (!used[i])
+                {
+                    used[i] = true;
+                    variations[index] = elements[i];
+                    Variations(index + 1);
+                    used[i] = false;
+                }
+            }
+        }
+    }
+}
