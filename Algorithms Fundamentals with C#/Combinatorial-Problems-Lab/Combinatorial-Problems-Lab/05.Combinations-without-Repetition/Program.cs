debe9fb70acdb7f4cdc40d5e8e6c9a5934448f43
Plamen P <pit_sofia@hotmail.com>
2022-06-19 15:09:44 +0300

    add tasks

---
+++ Algorithms Fundamentals with C#/Combinatorial-Problems-Lab/Combinatorial-Problems-Lab/05.Combinations-without-Repetition/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _05.Combinations_without_Repetition
+{
+    public class Program
+    {
+        private static string[] elements;
+        private static string[] combinations;
+
+        private static int k;
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split(' ');
+            k = int.Parse(Console.ReadLine());
+
+            combinations = new string[k];
+
+            Combination(0, 0);
+        }
+
+        private static void Combination(int index, int elementStartIdx)
+        {
+            if (index >= combinations.Length)
+            {
+                Console.WriteLine(string.Join(" ", combinations));
+                return;
+            }
+
+            for (int i = elementStartIdx; i < elements.Length; i++)
+            {
+                combinations[index] = elements[i];
+                Combination(index + 1, i + 1);
+            }
+        }
+    }
+}
---
+++ Algorithms Fundamentals with C#/Combinatorial-Problems-Lab/Combinatorial-Problems-Lab/06.Combinations-with-Repetition/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _06.Combinations_with_Repetition
+{
+    public class Program
+    {
+        private static string[] elements;
+        private static string[] combinations;
+
+        private static int k;
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split(' ');
+            k = int.Parse(Console.ReadLine());
+
+            combinations = new string[k];
+
+            CombinationWithRep(0, 0);
+        }
+
+        private static void CombinationWithRep(int index, int elementStartIndex)
+        {
+            if (index >= combinations.Length)
+            {
+                Console.WriteLine(string.Join(" ", combinations));
+                return;
+            }
+
+            for (int i = elementStartIndex; i < elements.Length; i++)
+            {
+                combinations[index] = elements[i];
+                CombinationWithRep(index + 1, i);
+            }
+        }
+    }
+}
