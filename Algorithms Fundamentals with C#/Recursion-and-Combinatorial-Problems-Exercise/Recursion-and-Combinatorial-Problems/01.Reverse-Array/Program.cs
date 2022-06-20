e45cbae3828dc3b2a66289bf48604d43f5879b20
Plamen P <pit_sofia@hotmail.com>
2022-06-20 19:15:41 +0300

    add exercise task

---
+++ Algorithms Fundamentals with C#/Recursion-and-Combinatorial-Problems-Exercise/Recursion-and-Combinatorial-Problems/01.Reverse-Array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.Reverse_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var elements = Console.ReadLine().Split().ToArray();
+
+            Reverse(elements, 0);
+
+            Console.WriteLine(string.Join(" ", elements));
+        }
+
+        private static void Reverse(string[] elements, int index)
+        {
+            if (index == elements.Length / 2)
+            {
+                return;
+            }
+
+            var temp = elements[index];
+            elements[index] = elements[elements.Length - index - 1];
+            elements[elements.Length - index - 1] = temp;
+
+            Reverse(elements, index + 1);
+        }
+    }
+}
