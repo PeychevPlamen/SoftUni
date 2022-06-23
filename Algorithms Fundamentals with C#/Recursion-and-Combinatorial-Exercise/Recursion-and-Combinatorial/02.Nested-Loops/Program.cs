158205d58392199628c3b45341cf1f2a855a1938
Plamen P <pit_sofia@hotmail.com>
2022-06-23 17:57:01 +0300

    some updates

---
+++ Algorithms Fundamentals with C#/Recursion-and-Combinatorial-Exercise/Recursion-and-Combinatorial/02.Nested-Loops/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _02.Nested_Loops
+{
+    public class Program
+    {
+        private static int[] elements;
+
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            elements = new int[n];
+
+            GenVectors(0);
+        }
+
+        private static void GenVectors(int index)
+        {
+            if (index >= elements.Length)
+            {
+                Console.WriteLine(String.Join(" ", elements));
+                return;
+            }
+
+            for (int i = 1; i <= elements.Length; i++)
+            {
+                elements[index] = i;
+                GenVectors(index + 1);
+            }
+        }
+    }
+}
