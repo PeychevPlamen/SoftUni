db92bd1bf446c8cd8770ca9bc12ca26b7c6eda0e
Plamen P <pit_sofia@hotmail.com>
2022-06-28 15:12:44 +0300

    finished task

--- Algorithms Fundamentals with C#/Recursion-and-Combinatorial-Ex/Recursion-and-Combinatorial/04.Cinema/Program.cs
+++ Algorithms Fundamentals with C#/Recursion-and-Combinatorial-Ex/Recursion-and-Combinatorial/04.Cinema/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace _04.Cinema
 {
@@ -19,7 +20,7 @@ namespace _04.Cinema
             while (true)
             {
                 var line = Console.ReadLine();
-
+                
                 if (line == "generate")
                 {
                     break;
@@ -27,13 +28,61 @@ namespace _04.Cinema
 
                 var parts = line.Split(" - ");
                 var name = parts[0];
-                var position = int.Parse(parts[1]);
+                var position = int.Parse(parts[1]) - 1;
 
                 people[position] = name;
                 locked[position] = true;
 
                 nonStatickNames.Remove(name);
+
             }
+
+            Permute(0);
+        }
+
+        private static void Permute(int idx)
+        {
+            if (idx >= nonStatickNames.Count)
+            {
+                PrintPermutation();
+                return;
+            }
+
+            Permute(idx + 1);
+
+            for (int i = idx + 1; i < nonStatickNames.Count; i++)
+            {
+                Swap(idx, i);
+                Permute(idx + 1);
+                Swap(idx, i);
+            }
+        }
+
+        private static void PrintPermutation()
+        {
+            var peopleIdx = 0;
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                if (locked[i])
+                {
+                   sb.Append($"{people[i]} ");
+                }
+                else
+                {
+                    sb.Append($"{nonStatickNames[peopleIdx++]} ");
+                }
+            }
+
+            Console.WriteLine(sb.ToString().Trim());
+        }
+
+        private static void Swap(int first, int second)
+        {
+            var temp = nonStatickNames[first];
+            nonStatickNames[first] = nonStatickNames[second];
+            nonStatickNames[second] = temp;
         }
     }
 }
