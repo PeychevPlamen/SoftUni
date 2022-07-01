f5f96b0d736ad3ff954a291edea7e37157853b9d
Plamen P <pit_sofia@hotmail.com>
2022-07-01 23:20:58 +0300

    update

---
+++ Algorithms Fundamentals with C#/Searching-Sorting-Greedy-Alg/Searching-Sorting-Greedy-Algorithms/Selection-Sort/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace Selection_Sort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            SelectionSort(input);
+
+            Console.WriteLine(string.Join(" ", input));
+        }
+
+        private static void SelectionSort(int[] input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                var min = i;
+
+                for (int j = i + 1; j < input.Length; j++)
+                {
+                    if (input[j] < input[min])
+                    {
+                        min = j;
+                    }
+                }
+
+                Swap(input, i, min);
+            }
+        }
+
+        private static void Swap(int[] input, int first, int second)
+        {
+            var temp = input[first];
+            input[first] = input[second];
+            input[second] = temp;
+        }
+    }
+}
