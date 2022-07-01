1a8dc16c05fa11444313e09638c3b574e296565e
Plamen P <pit_sofia@hotmail.com>
2022-07-01 22:26:22 +0300

    upload task 1

--- Algorithms Fundamentals with C#/Searching-Sorting-Greedy-Algorithms/Searching-Sorting-and-Greedy-Algorithms/01.Binary-Search/Program.cs
+++ Algorithms Fundamentals with C#/Searching-Sorting-Greedy-Algorithms/Searching-Sorting-and-Greedy-Algorithms/01.Binary-Search/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _01.Binary_Search
 {
@@ -6,7 +7,44 @@ namespace _01.Binary_Search
     {
         static void Main(string[] args)
         {
-            
+            var input = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .OrderBy(x => x)
+                .ToArray();
+
+            var key = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Binary_Search(input, key));
+        }
+
+        private static int Binary_Search(int[] input, int key)
+        {
+            var left = 0;
+            var right = input.Length - key;
+
+            while (left <= right)
+            {
+                var midle = (left + right) / 2;
+
+                var element = input[midle];
+
+                if (element == key)
+                {
+                    return midle;
+                }
+                if (key > element)
+                {
+                    left = midle + 1;
+                }
+                else
+                {
+                    right = midle - 1;
+                }
+
+            }
+
+            return -1;
         }
     }
 }
