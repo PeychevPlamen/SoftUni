450a455dc87f4526cb53cb5291f8db3e2ce0d0f2
Plamen P <pit_sofia@hotmail.com>
2022-07-05 15:50:45 +0300

    upload task 4

---
+++ Algorithms Fundamentals with C#/Searching-Sorting-Greedy-Alg/Searching-Sorting-Greedy-Algorithms/Insertion-Sort/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace Insertion_Sort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+               .Split()
+               .Select(int.Parse)
+               .ToArray();
+
+            InsertionSort(input);
+
+            Console.WriteLine(string.Join(" ", input));
+        }
+
+        private static void InsertionSort(int[] input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                var j = i;
+
+                while (j > 0 && input[j - 1] > input[j])
+                {
+                    Swap(input, j - 1, j);
+                    j--;
+                }
+            }
+        }
+        private static void Swap(int[] input, int first, int second)
+        {
+            var temp = input[first];
+            input[first] = input[second];
+            input[second] = temp;
+        }
+    }
+}
