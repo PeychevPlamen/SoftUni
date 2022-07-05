3b8852c76976bb9027cf2b966e5eca02361a7c4c
Plamen P <pit_sofia@hotmail.com>
2022-07-05 15:37:08 +0300

    upload task 3

---
+++ Algorithms Fundamentals with C#/Searching-Sorting-Greedy-Alg/Searching-Sorting-Greedy-Algorithms/03.Bubble-Sort/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Bubble_Sort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            BubbleSort(input);
+
+            Console.WriteLine(string.Join(" ", input));
+        }
+
+        private static void BubbleSort(int[] input)
+        {
+            var count = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var min = i;
+               
+                for (int j = i + 1; j < input.Length - count; j++)
+                {
+                    if (input[j] < input[min])
+                    {
+                        min = j;
+                    }
+                    if (j == input.Length - count)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+
+                Swap(input, i, min);
+            }
+        }
+        private static void Swap(int[] input, int first, int second)
+        {
+            var temp = input[first];
+            input[first] = input[second];
+            input[second] = temp;
+        }
+    }
+}
