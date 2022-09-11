da16270d95bca86a1bb59d1aa44412ecb394fcfb
Plamen P <pit_sofia@hotmail.com>
2022-09-11 20:46:04 +0300

    add some new tasks

--- Java Basics/First-Steps-In-Coding-Lab/src/InchesToCentimeters_04.java
+++ Java Basics/First-Steps-In-Coding-Lab/src/InchesToCentimeters_04.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class InchesToCentimeters_04 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        double input = Double.parseDouble(scanner.nextLine());
+        double inch = 2.54;
+
+        double output = input * inch;
+
+        System.out.println(output);
     }
 }
