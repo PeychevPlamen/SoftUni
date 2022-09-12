fa793dcdae124689939f736e62a431778a14decb
Plamen P <pit_sofia@hotmail.com>
2022-09-12 17:54:42 +0300

    add some new tasks

--- Java Basics/First-Steps-In-Coding-Lab/src/InchesToCentimeters_04.java
+++ Java Basics/First-Steps-In-Coding-Lab/src/InchesToCentimeters_04.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class InchesToCentimeters_04 {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
         double input = Double.parseDouble(scanner.nextLine());
