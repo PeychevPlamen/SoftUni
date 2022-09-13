465a8239678593775ad3eeee6c2e465491334c8b
Plamen P <pit_sofia@hotmail.com>
2022-09-13 20:49:54 +0300

    first steps in java exercise update

--- Java Basics/First-Steps-In-Coding-Exercise/src/DepositCalculator_03.java
+++ Java Basics/First-Steps-In-Coding-Exercise/src/DepositCalculator_03.java
@@ -8,7 +8,7 @@ public class DepositCalculator_03 {
         double deposit = Double.parseDouble(scanner.nextLine());
         int months = Integer.parseInt(scanner.nextLine());
         double interestRate = Double.parseDouble(scanner.nextLine());
-        
+
         Double sum = deposit + months * ((deposit * interestRate/100) / 12);
 
         System.out.printf("%.2f", sum);
