10ab2dacbcc59e8cf8682acd62905d5f00185786
Plamen P <pit_sofia@hotmail.com>
2022-09-18 17:58:49 +0300

    Conditional statements lab if-else

---
+++ Java Basics/Conditional-Statements-Lab/src/PasswordGuess_04.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PasswordGuess_04 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        String pass = "s3cr3t!P@ssw0rd";
+
+        if (input.equals(pass)) {
+            System.out.println("Welcome");
+        } else {
+            System.out.println("Wrong password!");
+        }
+    }
+}
