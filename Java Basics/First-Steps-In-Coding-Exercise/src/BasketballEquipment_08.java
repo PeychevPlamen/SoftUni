431754d14a43f6013656346cdca409964f16dac3
Plamen P <pit_sofia@hotmail.com>
2022-09-18 14:11:12 +0300

    first steps in java exercise update

--- Java Basics/First-Steps-In-Coding-Exercise/src/BasketballEquipment_08.java
+++ Java Basics/First-Steps-In-Coding-Exercise/src/BasketballEquipment_08.java
@@ -7,16 +7,12 @@ public class BasketballEquipment_08 {
 
         int price = Integer.parseInt(scanner.nextLine());
 
-        //•	Баскетболни кецове – цената им е 40% по-малка от таксата за една година
-        //•	Баскетболен екип – цената му е 20% по-евтина от тази на кецовете
-        //•	Баскетболна топка – цената ѝ е 1 / 4 от цената на баскетболния екип
-        //•	Баскетболни аксесоари – цената им е 1 / 5 от цената на баскетболната топка
         double sneakers = price * 0.6;
         double jersey = sneakers * 0.8;
         double ball = jersey * 0.25;
-        Double accessories = ball * 0.2;
+        double accessories = ball * 0.2;
 
-        Double total = price + sneakers + jersey + ball + accessories;
+        double total = price + sneakers + jersey + ball + accessories;
 
         System.out.println(total);
 
