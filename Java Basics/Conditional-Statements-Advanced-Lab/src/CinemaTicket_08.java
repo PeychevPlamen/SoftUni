0c43d0e5e68759ff8125e5c7c088d20bc286494a
Plamen P <pit_sofia@hotmail.com>
2022-09-28 17:47:31 +0300

    Conditional statements switch-case

---
+++ Java Basics/Conditional-Statements-Advanced-Lab/src/CinemaTicket_08.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class CinemaTicket_08 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String dayOfWeek = scanner.nextLine();
+        double price = 0;
+
+        switch (dayOfWeek){
+            case "Monday":
+            case "Tuesday":
+            case "Friday":
+                price = 12;
+                break;
+            case "Wednesday":
+            case "Thursday":
+                price = 14;
+                break;
+            case "Saturday":
+            case "Sunday":
+                price = 16;
+                break;
+        }
+        System.out.printf("%.0f", price);
+    }
+}
---
+++ Java Basics/Conditional-Statements-Advanced-Lab/src/WorkingHours_07.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class WorkingHours_07 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int time = Integer.parseInt(scanner.nextLine());
+        String dayOfWeek = scanner.nextLine();
+
+        if (time >= 10 && time <= 18) {
+            switch (dayOfWeek) {
+                case "Monday":
+                case "Tuesday":
+                case "Wednesday":
+                case "Thursday":
+                case "Friday":
+                case "Saturday":
+                    System.out.println("open");
+                    break;
+                default:
+                    System.out.println("closed");
+                    break;
+            }
+        } else {
+            System.out.println("closed");
+        }
+    }
+}
