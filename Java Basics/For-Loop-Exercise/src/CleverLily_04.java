7257a0d3e1e24c0368e20b6cadcdce93328da9a8
Plamen P <pit_sofia@hotmail.com>
2022-10-03 19:35:24 +0300

    For loop Exercise tasks

--- Java Basics/For-Loop-Exercise/src/CleverLily_04.java
+++ Java Basics/For-Loop-Exercise/src/CleverLily_04.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CleverLily_03 {
+public class CleverLily_04 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
