71a435ea50a564b4c0add981e7398bfee74d160a
Plamen P <pit_sofia@hotmail.com>
2023-09-14 16:19:07 +0300

    add more tasks

---
+++ Java Advanced/StacksAndQueues-Lab/Stacks-And-Queues-Lab/src/PrinterQueue_05.java
@@ -0,0 +1,32 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class PrinterQueue_05 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<String> printerQueue = new ArrayDeque<>();
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("print")) {
+
+            if (input.equals("cancel")) {
+                if (printerQueue.isEmpty()) {
+                    System.out.println("Printer is on standby");
+                } else {
+                    System.out.println("Canceled ".concat(printerQueue.pollFirst()));
+                }
+            } else {
+                printerQueue.offer(input);
+            }
+            input = scanner.nextLine();
+        }
+        while (!printerQueue.isEmpty()) {
+            System.out.println(printerQueue.poll());
+        }
+
+    }
+}
