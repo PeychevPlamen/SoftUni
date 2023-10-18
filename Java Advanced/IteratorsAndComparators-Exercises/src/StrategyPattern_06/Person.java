9a06c3ff0b3bd8ec3bb848bfe119a325863a0160
Plamen P <pit_sofia@hotmail.com>
2023-10-18 18:10:57 +0300

    Iterators and Comparators exercise

---
+++ Java Advanced/IteratorsAndComparators-Exercises/src/StrategyPattern_06/Person.java
@@ -0,0 +1,16 @@
+package StrategyPattern_06;
+
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %d", name, age);
+    }
+}
