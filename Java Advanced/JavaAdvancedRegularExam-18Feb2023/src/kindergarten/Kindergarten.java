0df6a0e2abb2e04d41b41e1e8720f15a5edb552a
Plamen P <pit_sofia@hotmail.com>
2023-10-20 15:22:07 +0300

    exam 18-Feb-2023 task 3 !!!

---
+++ Java Advanced/JavaAdvancedRegularExam-18Feb2023/src/kindergarten/Kindergarten.java
@@ -0,0 +1,69 @@
+package kindergarten;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Kindergarten {
+    private String name;
+    private int capacity;
+    private List<Child> registry;
+
+    public Kindergarten(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+        this.registry = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public boolean addChild(Child child) {
+        if (capacity > registry.size()) {
+            registry.add(child);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean removeChild(String firstName) {
+        return this.registry.removeIf(e -> e.getFirstName().equals(firstName));
+    }
+
+    public int getChildrenCount() {
+        return this.registry.size();
+    }
+
+    public Child getChild(String firstName) {
+        return this.registry.stream()
+                .filter(e -> e.getFirstName().equals(firstName))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public String registryReport() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Registered children in ").append(getName()).append(":");
+        Comparator<Child> childSort = Comparator
+                .comparing(Child::getAge)
+                .thenComparing(Child::getFirstName)
+                .thenComparing(Child::getLastName);
+        List<Child> sortedChild = registry.stream()
+                .sorted(childSort)
+                .collect(Collectors.toList());
+
+        sortedChild.forEach(child -> {
+            sb.append(System.lineSeparator());
+            sb.append("--");
+            sb.append(System.lineSeparator());
+            sb.append(child);
+        });
+        return sb.toString().trim();
+    }
+}
