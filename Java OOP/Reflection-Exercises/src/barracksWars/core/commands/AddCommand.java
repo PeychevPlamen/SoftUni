9b0cb8241e07ff0856c0c8f5bd4a7804480a25ec
Plamen P <pit_sofia@hotmail.com>
2023-11-18 19:19:47 +0200

    Reflection exercise tasks 1-4

---
+++ Java OOP/Reflection-Exercises/src/barracksWars/core/commands/AddCommand.java
@@ -0,0 +1,27 @@
+package barracksWars.core.commands;
+
+import barracksWars.interfaces.Repository;
+import barracksWars.interfaces.Unit;
+import barracksWars.interfaces.UnitFactory;
+import jdk.jshell.spi.ExecutionControl;
+
+public class AddCommand extends Command {
+    public AddCommand(String[] data, Repository repository, UnitFactory unitFactory) {
+        super(data, repository, unitFactory);
+    }
+
+    @Override
+    public String execute() {
+        String unitType = this.getData()[1];
+        Unit unitToAdd = null;
+        try {
+            unitToAdd = this.getUnitFactory().createUnit(unitType);
+        } catch (ExecutionControl.NotImplementedException e) {
+            e.printStackTrace();
+        }
+
+        this.getRepository().addUnit(unitToAdd);
+        String output = unitType + " added!";
+        return output;
+    }
+}
