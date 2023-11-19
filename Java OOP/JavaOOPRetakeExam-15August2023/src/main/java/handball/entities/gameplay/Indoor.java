7c58763d136b53489d6accbf62b087deee97da51
Plamen P <pit_sofia@hotmail.com>
2023-11-19 15:51:58 +0200

    Java OOP Retake Exam - 15 August 2023 task 1

--- Java OOP/JavaOOPRetakeExam-15August2023/src/main/java/handball/entities/gameplay/Indoor.java
+++ Java OOP/JavaOOPRetakeExam-15August2023/src/main/java/handball/entities/gameplay/Indoor.java
@@ -1,10 +1,5 @@
 package handball.entities.gameplay;
 
-import handball.entities.equipment.Equipment;
-import handball.entities.team.Team;
-
-import java.util.Collection;
-
 public class Indoor extends BaseGameplay{
     private static final int CAPACITY = 250;
     public Indoor(String name) {
--- Java OOP/JavaOOPRetakeExam-15August2023/src/main/java/handball/entities/gameplay/Outdoor.java
+++ Java OOP/JavaOOPRetakeExam-15August2023/src/main/java/handball/entities/gameplay/Outdoor.java
@@ -1,10 +1,5 @@
 package handball.entities.gameplay;
 
-import handball.entities.equipment.Equipment;
-import handball.entities.team.Team;
-
-import java.util.Collection;
-
 public class Outdoor extends BaseGameplay{
 private static final int CAPACITY = 150;
     public Outdoor(String name) {
