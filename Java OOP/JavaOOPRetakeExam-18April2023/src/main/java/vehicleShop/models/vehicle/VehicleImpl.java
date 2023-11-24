5126a57f82a4fa67db040f3f410581c9e742544d
Plamen P <pit_sofia@hotmail.com>
2023-11-24 17:36:52 +0200

    Retake Exam 18-April-2023 Business Logic

---
+++ Java OOP/JavaOOPRetakeExam-18April2023/src/main/java/vehicleShop/models/vehicle/VehicleImpl.java
@@ -0,0 +1,51 @@
+package vehicleShop.models.vehicle;
+
+import static vehicleShop.common.ExceptionMessages.VEHICLE_NAME_NULL_OR_EMPTY;
+import static vehicleShop.common.ExceptionMessages.VEHICLE_STRENGTH_LESS_THAN_ZERO;
+
+public class VehicleImpl implements Vehicle {
+    private String name;
+    private int strengthRequired;
+
+    public VehicleImpl(String name, int strengthRequired) {
+        setName(name);
+        setStrengthRequired(strengthRequired);
+    }
+
+    public void setName(String name) {
+        if (name == null || name.equals("")) {
+            throw new IllegalArgumentException(VEHICLE_NAME_NULL_OR_EMPTY);
+        }
+        this.name = name;
+    }
+
+    public void setStrengthRequired(int strengthRequired) {
+        if (strengthRequired < 0) {
+            throw new IllegalArgumentException(VEHICLE_STRENGTH_LESS_THAN_ZERO);
+        }
+        this.strengthRequired = strengthRequired;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getStrengthRequired() {
+        return strengthRequired;
+    }
+
+    @Override
+    public boolean reached() {
+        return getStrengthRequired() == 0;
+    }
+
+    @Override
+    public void making() {
+        setStrengthRequired(getStrengthRequired() - 5);
+        if (getStrengthRequired() < 0) {
+            setStrengthRequired(0);
+        }
+    }
+}
