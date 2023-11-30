60d357b3c9b69572ec62c0f4133770aabcb2ddfd
Plamen P <pit_sofia@hotmail.com>
2023-11-30 19:18:27 +0200

    java oop exam 10Dec2022 High Quality Structure

---
+++ Java OOP/JavaOOPRegularExam-10December2022/ChristmasStoreApp/src/main/java/christmasPastryShop/entities/booths/interfaces/BaseBooth.java
@@ -0,0 +1,90 @@
+package christmasPastryShop.entities.booths.interfaces;
+
+import christmasPastryShop.entities.cocktails.interfaces.Cocktail;
+import christmasPastryShop.entities.delicacies.interfaces.Delicacy;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static christmasPastryShop.common.ExceptionMessages.INVALID_NUMBER_OF_PEOPLE;
+import static christmasPastryShop.common.ExceptionMessages.INVALID_TABLE_CAPACITY;
+
+public abstract class BaseBooth implements Booth {
+    private Collection<Delicacy> delicacyOrders;
+    private Collection<Cocktail> cocktailOrders;
+    private int boothNumber;
+    private int capacity;
+    private int numberOfPeople;
+    private double pricePerPerson;
+    private boolean isReserved;
+    private double price;
+
+    public BaseBooth(int boothNumber, int capacity, double pricePerPerson) {
+        this.boothNumber = boothNumber;
+        setCapacity(capacity);
+        this.pricePerPerson = pricePerPerson;
+        this.isReserved = false;
+        this.delicacyOrders = new ArrayList<>();
+        this.cocktailOrders = new ArrayList<>();
+        this.price = 0;
+    }
+
+    private void setCapacity(int capacity) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException(INVALID_TABLE_CAPACITY);
+        }
+        this.capacity = capacity;
+    }
+
+    private void setNumberOfPeople(int numberOfPeople) {
+        if (numberOfPeople <= 0) {
+            throw new IllegalArgumentException(INVALID_NUMBER_OF_PEOPLE);
+        }
+        this.numberOfPeople = numberOfPeople;
+    }
+
+    @Override
+    public int getBoothNumber() {
+        return boothNumber;
+    }
+
+    @Override
+    public int getCapacity() {
+        return capacity;
+    }
+
+    @Override
+    public boolean isReserved() {
+        return isReserved;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public void reserve(int numberOfPeople) {
+        setNumberOfPeople(numberOfPeople);
+        isReserved = true;
+        price = pricePerPerson * numberOfPeople;
+    }
+
+    @Override
+    public double getBill() {
+        double bill = 0;
+        bill += delicacyOrders.stream().mapToDouble(Delicacy::getPrice).sum();
+        bill += cocktailOrders.stream().mapToDouble(Cocktail::getPrice).sum();
+        bill += getPrice(); // ????
+        return bill;
+    }
+
+    @Override
+    public void clear() {
+        cocktailOrders.clear();
+        delicacyOrders.clear();
+        setNumberOfPeople(0);
+        price = 0;
+        isReserved = false;
+    }
+}
---
+++ Java OOP/JavaOOPRegularExam-10December2022/ChristmasStoreApp/src/main/java/christmasPastryShop/entities/delicacies/interfaces/BaseDelicacy.java
@@ -0,0 +1,56 @@
+package christmasPastryShop.entities.delicacies.interfaces;
+
+import static christmasPastryShop.common.ExceptionMessages.*;
+
+public abstract class BaseDelicacy implements Delicacy{
+    private String name;
+    private double portion;
+    private double price;
+
+    public BaseDelicacy(String name, double portion, double price) {
+        setName(name);
+        setPortion(portion);
+        setPrice(price);
+    }
+
+    private void setName(String name) {
+        if (name == null || name.isBlank()){
+            throw new IllegalArgumentException(INVALID_NAME);
+        }
+        this.name = name;
+    }
+
+    private void setPortion(double portion) {
+        if (portion <= 0){
+            throw new IllegalArgumentException(INVALID_PORTION);
+        }
+        this.portion = portion;
+    }
+
+    private void setPrice(double price) {
+        if (price <= 0){
+            throw new IllegalArgumentException(INVALID_PRICE);
+        }
+        this.price = price;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public double getPortion() {
+        return portion;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: %.2f - %.2f", getName(), getPortion(), getPrice());
+    }
+}
